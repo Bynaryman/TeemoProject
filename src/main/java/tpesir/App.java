a86701593e06b94dbafe31423aa0085b35307a90
teemo <dd@gmail.com>
2017-02-13 15:48:18 +0100

    init commit

---
+++ src/main/java/tpesir/App.java
@@ -0,0 +1,13 @@
+package tpesir;
+
+/**
+ * Hello world!
+ *@author 14000860 LOUIS
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
